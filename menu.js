fb17419a8cbd4323b267458aaabea1f9dbdfc2be
eduardo dourado <165962217+douradosD@users.noreply.github.com>
2024-04-15 08:33:55 -0300

    Initial commit

---
+++ menu.js
@@ -0,0 +1,7 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
